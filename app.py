e7f4934e03fa75eda702c96a8536940155e71bba
Lukas Cedronas <lukascedronas1@gmail.com>
2019-05-12 02:20:23 +0300

    segment shoes by color. refactor

--- app.py
+++ app.py
@@ -4,7 +4,24 @@ from matplotlib import pyplot as plt
 from skimage.measure import compare_ssim
 from PIL import Image
 
-img = cv2.imread('hand-ball1.jpg')
+def morphOpen(image):
+	return cv2.morphologyEx(image, cv2.MORPH_OPEN, np.ones((3,3),np.uint8))
+
+def morphDilate(image):
+	return cv2.morphologyEx(image, cv2.MORPH_DILATE, np.ones((3,3),np.uint8))
+
+def erode(image, iterations):
+	return cv2.erode(image, np.ones((3,3), np.uint8), iterations=iterations)
+
+def dilate(image, iterations):
+	return cv2.dilate(image, np.ones((3,3), np.uint8), iterations=iterations)
+
+def printImg(image):
+	cv2.namedWindow('image',cv2.WINDOW_NORMAL)
+	cv2.resizeWindow('image', 600,600)
+	cv2.imshow("image", image)
+
+img = cv2.imread('kursiniui.png')
 
 # Laterally invert the image / flip the image
 img  = cv2.flip(img, 1);
@@ -12,51 +29,48 @@ img  = cv2.flip(img, 1);
 # converting from BGR to HSV color space
 hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 # Range for lower orange
-lower_red = np.array([2,150,150])
-upper_red = np.array([7,255,255])
-maskForBall1 = cv2.inRange(hsv, lower_red, upper_red)
- 
-# Range for upper range
-lower_red = np.array([170,150,150])
-upper_red = np.array([180,255,255])
-maskForBall2 = cv2.inRange(hsv,lower_red,upper_red)
- 
-# Generating the final mask to detect orange
-maskForBall1 = maskForBall1+maskForBall2
+lower_green = np.array([77, 60, 50])
+upper_green = np.array([83, 255, 200])
+maskForBall = cv2.inRange(hsv, lower_green, upper_green)
 
 # Range for hand color
-lower_red = np.array([10,50,150])
-upper_red = np.array([50,200,255])
-maskForHand1 = cv2.inRange(hsv, lower_red, upper_red)
+lower_yellow = np.array([19,130,130])
+upper_yellow = np.array([25,255,255])
+maskForHand = cv2.inRange(hsv, lower_yellow, upper_yellow)
 
-maskForBall1 = cv2.morphologyEx(maskForBall1, cv2.MORPH_OPEN, np.ones((3,3),np.uint8))
-maskForBall1 = cv2.morphologyEx(maskForBall1, cv2.MORPH_DILATE, np.ones((3,3),np.uint8))
+# Range for shoes color
+lower_red = np.array([173,100,100])
+upper_red = np.array([177,250,255])
+maskForShoes = cv2.inRange(hsv, lower_red, upper_red)
 
-maskForHand1 = cv2.morphologyEx(maskForHand1, cv2.MORPH_OPEN, np.ones((3,3),np.uint8))
-maskForHand1 = cv2.morphologyEx(maskForHand1, cv2.MORPH_DILATE, np.ones((3,3),np.uint8))
+maskForBall = morphDilate(morphOpen(maskForBall))
+maskForHand = morphDilate(morphOpen(maskForHand))
+maskForShoes =  morphDilate(morphOpen(maskForShoes))
  
 # creating an inverted mask to segment out the ball from the rest of the frame
-maskForBall2 = cv2.bitwise_not(maskForBall1)
-maskForHand1 = cv2.bitwise_and(maskForHand1, maskForBall2)
+maskForNotBall = cv2.bitwise_not(maskForBall)
+maskForHand = cv2.bitwise_and(maskForHand, maskForNotBall)
 
-dilated1 = cv2.dilate(maskForBall1, np.ones((3,3), np.uint8), iterations=15)
-eroded1 = cv2.erode(dilated1, np.ones((3,3), np.uint8), iterations=3)
-dilated2 = cv2.dilate(maskForHand1, np.ones((3,3), np.uint8), iterations=7)
-eroded2 = cv2.erode(dilated2, np.ones((3,3), np.uint8), iterations=3)
-contours1, h = cv2.findContours(eroded1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-contours2, h = cv2.findContours(eroded2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+maskForBall = erode(dilate(maskForBall, 15), 3)
+maskForHand = dilate(erode(maskForHand, 3), 7)
+maskForShoes = erode(dilate(maskForShoes, 7), 3)
 
-(x,y),radius = cv2.minEnclosingCircle(contours1[0])
+ballContours, h = cv2.findContours(maskForBall, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+handContours, h = cv2.findContours(maskForHand, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+shoeContours, h = cv2.findContours(maskForShoes, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+(x,y),radius = cv2.minEnclosingCircle(ballContours[0])
 center = (int(x),int(y))
-cv2.circle(eroded1,center,int(radius),(255,0,0), -1)
+cv2.circle(maskForBall,center,int(radius),(255,0,0), -1)
 
-cv2.fillPoly(eroded2, color = (255, 0, 0) , pts = contours2 )
+# fill hand
+cv2.fillPoly(maskForHand, color = (255, 0, 0), pts = handContours )
 
-eroded1 = cv2.bitwise_and(eroded1,eroded1,mask=eroded2)
-eroded1 = np.array(eroded1)
+handAndBall = cv2.bitwise_and(maskForBall,maskForBall,mask=maskForHand)
+handAndBall = np.array(handAndBall)
+printImg(handAndBall)
 
-pixelPctg = eroded1[np.where(eroded1 >= 1)].size / eroded1.size * 100
-print(pixelPctg)
+pixelPctg = handAndBall[np.where(handAndBall >= 1)].size / handAndBall.size * 100
 
 if pixelPctg > 0.1: 
     print("rankoje")
