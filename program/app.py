7b5b254d414cb4c5897f8c67a9bd489214340e87
Lukas Cedronas <lukascedronas1@gmail.com>
2019-05-13 00:26:30 +0300

    program structure update

---
+++ program/app.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+from skimage.measure import compare_ssim
+from PIL import Image
+from Algorithm import Algorithm
+from ImageProcessingUtils import print_img
+	
+vid = cv2.VideoCapture('../kursiniuivid(0).mp4')
+algorithm = Algorithm()
+
+while(True):
+    # Capture frame-by-frame
+	ret, frame = vid.read()
+	frame = algorithm.execute(frame)
+
+    # Display the resulting frame
+	print_img(frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+# When everything done, release the capture
+vid.release()
+cv2.destroyAllWindows()
